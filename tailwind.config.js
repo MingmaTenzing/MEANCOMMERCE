c22cd4edd9b355f5d70d5059029e9c033b23ab48
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-03-18 10:41:49 +0800

    working on the bottom nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ciliconorange: "#FA8232",
+      },
+    },
   },
   plugins: [],
 };
