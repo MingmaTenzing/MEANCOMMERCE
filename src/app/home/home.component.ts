8d6241f7cb3216d28396ef039132411b4fba7932
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-03-19 23:20:33 +0800

    working on featured

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { WidgetsComponent } from './widgets/widgets.component';
+import { FeaturesComponent } from './features/features.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WidgetsComponent],
+  imports: [WidgetsComponent, FeaturesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
