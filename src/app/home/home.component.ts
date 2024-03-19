35c6da9ba3916ff6a4fe78c77ad2a5601cbd0bfa
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-03-19 14:51:41 +0800

    widgets complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { WidgetsComponent } from './widgets/widgets.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [WidgetsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
