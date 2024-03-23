75970b689d0f597d63ad2f5a18a6ecc6eb2eac40
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-03-23 11:32:21 +0800

    shop_category

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { WidgetsComponent } from './widgets/widgets.component';
 import { FeaturesComponent } from './features/features.component';
 import { BestdealsComponent } from './bestdeals/bestdeals.component';
+import { ShopCategoryComponent } from './shop-category/shop-category.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [WidgetsComponent, FeaturesComponent, BestdealsComponent],
+  imports: [
+    WidgetsComponent,
+    FeaturesComponent,
+    BestdealsComponent,
+    ShopCategoryComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
---
+++ src/app/home/shop-category/shop-category.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-category',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-category.component.html',
+  styleUrl: './shop-category.component.css'
+})
+export class ShopCategoryComponent {
+
+}
