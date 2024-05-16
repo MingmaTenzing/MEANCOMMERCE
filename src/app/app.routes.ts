ffc633399b6c3c375e4ee072754b7779415946b1
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-05-16 11:08:15 +0800

    skeleton loading complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { UploadProductsComponent } from './upload-products/upload-products.component';
 import { HomeComponent } from './home/home.component';
-import { TestComponent } from './test/test.component';
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -10,9 +9,9 @@ export const routes: Routes = [
     path: 'upload',
     component: UploadProductsComponent,
   },
-  {
-    path: 'test',
-    component: TestComponent,
-  },
+  // {
+  //   path: 'test',
+  //   component: TestComponent,
+  // },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
