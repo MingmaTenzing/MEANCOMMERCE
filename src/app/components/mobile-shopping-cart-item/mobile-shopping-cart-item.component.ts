06f4280cdf43e787eaa81f9a1b4b86ec40cce962
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-06-28 09:39:08 +0800

    cart design for mobile almost done..

--- src/app/components/mobile-shopping-cart-item/mobile-shopping-cart-item.component.ts
+++ src/app/components/mobile-shopping-cart-item/mobile-shopping-cart-item.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { cartItems } from '../../../types';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-mobile-shopping-cart-item',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './mobile-shopping-cart-item.component.html',
   styleUrl: './mobile-shopping-cart-item.component.css',
 })
