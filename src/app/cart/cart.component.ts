3470de75d44c0ffcbc6b18be1e8bd020ec23a99e
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-07-03 15:36:13 +0800

    working on cart state

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MobileShoppingCartItemComponent } from '../components/mobile-shopping-cart-item/mobile-shopping-cart-item.component';
 import { CartService } from '../../services/cart/cart.service';
 import { cartItems } from '../../types';
@@ -13,8 +13,8 @@ import { Subject, filter, takeUntil } from 'rxjs';
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
-export class CartComponent {
-  _destroy$ = new Subject();
+export class CartComponent implements OnDestroy {
+  private _destroy$ = new Subject<void>();
   cartItems: cartItems[] = [];
 
   constructor(private cartService: CartService) {
@@ -22,5 +22,9 @@ export class CartComponent {
       .getCartItems()
       .pipe(takeUntil(this._destroy$))
       .subscribe((data) => (this.cartItems = data));
+  }
+
+  ngOnDestroy(): void {
+    this._destroy$.next();
   }
 }
