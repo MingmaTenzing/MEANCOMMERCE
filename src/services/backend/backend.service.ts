e1afa9a1b3e92053fda149289cc277a449f3b155
Tenzing Sherpa <pasangsherpa689@gmail.com>
2024-11-05 11:59:45 +0800

    updated backend service

--- src/services/backend/backend.service.ts
+++ src/services/backend/backend.service.ts
@@ -11,7 +11,7 @@ import { FormGroup } from '@angular/forms';
 import { auth_session } from '../../types';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class BackendService {
   destroyRef = inject(DestroyRef);
@@ -20,20 +20,20 @@ export class BackendService {
 
   getData() {
     return this.http.get<MeanProducts[]>(
-      'http://localhost:5000/api/v1/products?page=1&limit=10'
+      `${process.env.HOST}/api/v1/products?page=1&limit=10`
     );
   }
 
   getshopProducts(filter: FilterSearch) {
     return this.http.post<MeanProducts[]>(
-      'http://localhost:5000/api/v1/products?page=1&limit=10',
+      `${process.env.HOST}/api/v1/products?page=1&limit=10`,
       filter
     );
   }
 
   getCategoryProducts(categoryName: string) {
     return this.http.post<MeanProducts[]>(
-      'http://localhost:5000/api/v1/products/category-products?page=1&limit=10',
+      `${process.env.HOST}/api/v1/products/category-products?page=1&limit=10`,
       {
         category: categoryName,
       }
@@ -42,13 +42,13 @@ export class BackendService {
 
   getSingleProduct(id: string) {
     return this.http.get<MeanProducts>(
-      `http://localhost:5000/api/v1/products/${id}`
+      `${process.env.HOST}/api/v1/products/${id}`
     );
   }
 
   signInUser(user_credentials: FormGroup) {
     return this.http.post<user>(
-      'http://localhost:5000/api/v1/auth/login',
+      `${process.env.HOST}/api/v1/auth/login`,
       {
         email: user_credentials.value.email,
         password: user_credentials.value.password,
@@ -61,7 +61,7 @@ export class BackendService {
 
   signup_user(user_details: FormGroup) {
     return this.http.post(
-      'http://localhost:5000/api/v1/auth/register',
+      `${process.env.HOST}/api/v1/auth/register`,
       {
         email: user_details.value.email,
         password: user_details.value.password,
@@ -80,7 +80,7 @@ export class BackendService {
     console.log(formData);
 
     return this.http.post<uploadImage>(
-      'http://localhost:5000/api/v1/upload-image',
+      `${process.env.HOST}/api/v1/upload-image`,
       formData,
 
       {
@@ -91,7 +91,7 @@ export class BackendService {
 
   check_session() {
     return this.http.get<auth_session>(
-      'http://localhost:5000/api/v1/auth/auth-check-session',
+      `${process.env.HOST}/api/v1/auth/auth-check-session`,
       {
         withCredentials: true,
       }
@@ -100,7 +100,7 @@ export class BackendService {
 
   fetch_user_details() {
     return this.http.get<user>(
-      'http://localhost:5000/api/v1/dashboard/user-details',
+      `${process.env.HOST}/api/v1/dashboard/user-details`,
       {
         withCredentials: true,
       }
@@ -108,14 +108,14 @@ export class BackendService {
   }
 
   log_out_user() {
-    return this.http.get('http://localhost:5000/api/v1/dashboard/logout', {
+    return this.http.get(`${process.env.HOST}/api/v1/dashboard/logout`, {
       withCredentials: true,
     });
   }
 
   get_recent_orders() {
     return this.http.get<orders[]>(
-      'http://localhost:5000/api/v1/orders/get_recent_orders',
+      `${process.env.HOST}/api/v1/orders/get_recent_orders`,
       {
         withCredentials: true,
       }
