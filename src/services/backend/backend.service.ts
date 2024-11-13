3e44c5de6b88b8c86dcaa5761a7af22cbb743f45
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-11-13 11:30:35 +0800

    need to work on product detail page

--- src/services/backend/backend.service.ts
+++ src/services/backend/backend.service.ts
@@ -1,5 +1,5 @@
-import { inject, Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import {
   FilterSearch,
   MeanProducts,
