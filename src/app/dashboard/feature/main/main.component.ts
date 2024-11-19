01a6ec8e59b258d222c7cce51517e26ea37bf531
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-11-19 10:28:55 +0800

    wishlist layout fix

--- src/app/dashboard/feature/main/main.component.ts
+++ src/app/dashboard/feature/main/main.component.ts
@@ -2,13 +2,13 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AccountInfoCardComponent } from '../../components/account-info-card/account-info-card.component';
 import { BillingAddressCardComponent } from '../../components/billing-address-card/billing-address-card.component';
 import { FunFactCardsComponent } from '../../components/fun-fact-cards/fun-fact-cards.component';
-import { PaymentOptionComponent } from '../../components/payment-option/payment-option.component';
 import { RecentOrderComponent } from '../../components/recent-order/recent-order.component';
 import { BackendService } from '../../../../services/backend/backend.service';
 import { Observable, Subject, takeUntil } from 'rxjs';
 import { user } from '../../../../types';
 import { CommonModule } from '@angular/common';
 import { MainLoadingComponent } from '../../components/loading/main-loading/main-loading.component';
+import { PaymentOptionComponent } from '../../components/payment-option/payment-option.component';
 
 @Component({
   selector: 'app-main',
