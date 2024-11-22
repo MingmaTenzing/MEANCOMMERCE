7cd9225af3c1918f80bd297e127de38093b60c0b
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-11-22 09:16:04 +0800

    removed ngx cookie no need anymore

--- src/app/dashboard/components/side-menu/side-menu.component.ts
+++ src/app/dashboard/components/side-menu/side-menu.component.ts
@@ -8,20 +8,15 @@ import {
 } from '@angular/router';
 import { BackendService } from '../../../../services/backend/backend.service';
 import { user } from '../../../../types';
-import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-side-menu',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css',
-  providers: [CookieService],
 })
 export class SideMenuComponent {
-  cookieService = inject(CookieService);
-  constructor(private backend: BackendService, private router: Router) {
-    console.log(this.cookieService.get('token'));
-  }
+  constructor(private backend: BackendService, private router: Router) {}
   logOut() {
     this.backend.log_out_user().subscribe((data) => {
       if (data) {
