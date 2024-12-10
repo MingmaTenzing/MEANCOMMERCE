9bb70b275175e54198bdb71b22ae6f054e506f53
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-12-10 09:44:41 +0800

    ai-assistant route is established

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,6 +25,7 @@ import { SignupComponent } from './auth/feature/signup/signup/signup.component';
 import { AuthFormComponent } from './auth/auth-form/auth-form.component';
 import { CartComponent } from './cart/cart.component';
 import { dashBoardGuard } from '../guard/auth/auth.guard';
+import { AiAssistantComponent } from './ai-assistant/ai-assistant.component';
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
 
@@ -62,6 +63,11 @@ export const routes: Routes = [
     path: 'wishlist',
     component: WishlistComponent,
     title: 'Wishlist',
+  },
+  {
+    path: 'ai-assistant',
+    component: AiAssistantComponent,
+    title: 'AI Assistant',
   },
   {
     path: 'dashboard',
