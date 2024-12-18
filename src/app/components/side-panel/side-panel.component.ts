3da0234fb97dbeef07c46903da28f5c37f452b64
Mingma Tenzing Sherpa <mingmatenzingsherpa@Mingmas-Mac-mini.local>
2024-12-18 11:17:24 +0800

    prod push

--- src/app/components/side-panel/side-panel.component.ts
+++ src/app/components/side-panel/side-panel.component.ts
@@ -1,13 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import {
+  Router,
+  RouterLinkActive,
+  RouterModule,
+  RouterOutlet,
+} from '@angular/router';
+import { Console } from 'console';
 @Component({
   selector: 'app-side-panel',
-  imports: [SidebarModule, ButtonModule],
+  imports: [
+    SidebarModule,
+    ButtonModule,
+    RouterModule,
+    CommonModule,
+    RouterModule,
+    RouterOutlet,
+    RouterLinkActive,
+  ],
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.css',
 })
 export class SidePanelComponent {
   @Input() sidebarVisible: boolean = false;
+  constructor(private router: Router) {}
+
+  gotoCart() {
+    this.router.navigate(['/cart']);
+  }
+  gotoWishlist() {
+    this.router.navigate(['/wishlist']);
+  }
 }
